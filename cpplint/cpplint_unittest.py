8798a7699f1443c3581a3aeacda5ec3c9e83262d
Martin Zink <martinzink@apache.org>
2023-01-12 13:05:34 +0100

    fix: #752 cpplint false positive [readability/braces] with requires expression (C++20)

--- cpplint/cpplint_unittest.py
+++ cpplint/cpplint_unittest.py
@@ -2534,6 +2534,7 @@ class CpplintTest(CpplintTestBase):
     self.TestLint('class X : public Y {};', '')
     self.TestLint('class X : public MACRO() {};', '')
     self.TestLint('class X : public decltype(expr) {};', '')
+    self.TestLint('concept Addable = requires(T x) { x + x; };', '')
     self.TestLint('DEFINE_FACADE(PCQueue::Watcher, PCQueue) {};', '')
     self.TestLint('VCLASS(XfaTest, XfaContextTest) {};', '')
     self.TestLint('class STUBBY_CLASS(H, E) {};', '')
